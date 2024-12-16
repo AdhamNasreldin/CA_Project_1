6bcf118e7e026fb2745ee1160330038f86d2c305
Adham Nasreldin <adhamnasreldin16@gmail.com>
2024-12-17 01:57:08 +0200

    Instruction Memory

---
+++ InstructionMemory.v
@@ -0,0 +1,5 @@
+module InstructionMemory( input [31:0] ReadAddress , output [31:0] ReadData );
+    reg [7:0] Memory [4095:0];
+
+    assign ReadData = {Memory[ReadAddress], Memory[ReadAddress+1], Memory[ReadAddress+2], Memory[ReadAddress+3]};
+endmodule

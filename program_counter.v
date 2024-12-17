af93535f4083be8c0513fe3b0282259e78008abb
Adham Nasreldin <adhamnasreldin16@gmail.com>
2024-12-17 22:15:47 +0200

    program counter v2

--- program_counter.v
+++ program_counter.v
@@ -1,12 +1,14 @@
 module ProgramCounter (
-    input clk, reset,
-    input [31:0] next_pc,
+    input clk, reset,,branch ,[15:0] branch_offset
+    input [31:0] pc_in,
     output reg [31:0] pc
 );
     always @(posedge clk or posedge reset) begin
         if (reset)
             pc <= 0;
+        else if (branch)
+             pc <= pc + 4 + (branch_offset << 2); // PC + 4 + branch offset
         else
-            pc <= next_pc;
+            pc <= pc_in;
     end
 endmodule
